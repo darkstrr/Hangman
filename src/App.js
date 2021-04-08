762948b4b7b9b4f061eb8a871756e7357557af16
Albert <ec2-user@ip-172-31-25-46.ec2.internal>
2021-04-08 21:00:46 +0000

    one person Hangman

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import Header from './Header';
 import Figure from './Figure';
 import WrongLetters from './Incorrect';
 import Word from './Word';
-import Notify from './Notify';
-import Popup from './Popup';
 import { showNotification as show, checkWin } from './helpers';
 import io from 'socket.io-client';
 
@@ -16,47 +14,14 @@ const words = []
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
-  const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [showNotify, setShowNotify] = useState(false);
   
   useEffect(() => {
-    const handleKeydown = event => {
-      const { key, keyCode } = event;
-      if (playable && keyCode >= 65 && keyCode <= 90) {
-        const letter = key.toLowerCase();
-        if (selectedWord.includes(letter)) {
-          if (!correctLetters.includes(letter)) {
-            setCorrectLetters(currentLetters => [...currentLetters, letter]);
-          } else {
-            show(setShowNotify);
-          }
-        } else {
-          if (!wrongLetters.includes(letter)) {
-            setWrongLetters(currentLetters => [...currentLetters, letter]);
-          } else {
-            show(setShowNotify);
-          }
-        }
-      }
-      socket.emit('update', {playable, correctLetters, wrongLetters, setShowNotify})
-    }
-    window.addEventListener('keydown', handleKeydown);
+    
+  }, []);
 
-    return () => window.removeEventListener('keydown', handleKeydown);
-  }, [correctLetters, wrongLetters, playable]);
 
-  function playAgain() {
-      setPlayable(true);
-  
-      // Empty Arrays
-      setCorrectLetters([]);
-      setWrongLetters([]);
-  
-      const random = Math.floor(Math.random() * words.length);
-      selectedWord = words[random];
-    }
     
   return (
     <div>
@@ -65,8 +30,6 @@ function App() {
           <WrongLetters wrongLetters={wrongLetters} />
           <Word selectedWord={selectedWord} correctLetters={correctLetters} />
         </div>
-      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
-      <Notify showNotify={showNotify} />
       
     </div>
   );
