f9adce488c52b232e72bd2993982e755d07b0785
michaelcbarilla <michael@eeproto.com>
2021-04-16 11:29:32 -0400

    add logic to make sure you can only input one letter into the guesser

--- src/Guesser.js
+++ src/Guesser.js
@@ -15,15 +15,23 @@ function Guesser(props) {
       props.socket.emit('guess', guess);
     }
   }
+
+  function handleChange(e) {
+    if (e.currentTarget.value.length > 1) {
+      e.currentTarget.value = e.currentTarget.value[0];
+    }
+  };
+
   return (
     <div className="guesser-container">
-      <label htmlFor="basic-url">Enter a Letter! (must be one letter)</label>
+      <label htmlFor="basic-url">Enter a Letter!</label>
       <InputGroup className="mb-3">
         <FormControl
           ref={inputRef} 
           type="char" 
           aria-label="guess a letter"
           aria-describedby="basic-addon2"
+          onChange={handleChange}
         />
       </InputGroup>
       <Button variant="secondary" onClick={() => onGuess()}>Submit</Button>
