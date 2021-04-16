bc66237205774dd3efb590029195c3ca30928e13
michaelcbarilla <michael@eeproto.com>
2021-04-16 12:03:12 -0400

    add logic to handle selected category/switch ui upon selecting a category

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const socket = io();
 
@@ -20,11 +21,16 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 function App() {
   const [categories, setCategories] = useState([]);
-  const [categorySelected, setCategorySelected] = useState(false);
+  const [categorySelected, setCategorySelected] = useState(null);
   const [guess, setGuess] = useState([]);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const inputRef = useRef(null);
+
+  const selectRef = useRef(null);
+
+  function onSelectCategory() {
+    setCategorySelected(selectRef.current.value);
+  }
   
   
   useEffect(() => {
@@ -48,14 +54,19 @@ function App() {
         </Row>
         <Row className="mt-5 w-25 m-auto">
           <Col>
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>Categories</Form.Label>
-            <Form.Control as="select">
-              {categories.map(category => (
-                <option>{category}</option>
-              ))}
-            </Form.Control>
-          </Form.Group>
+            <Form>
+              <Form.Group as={Col} controlId="formGridState">
+                <Form.Label>Categories</Form.Label>
+                <Form.Control as="select" ref={selectRef}>
+                  {categories.map((category, i) => (
+                    <option key={i}>{category}</option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+              <Button variant="light" type="button" onClick={onSelectCategory}>
+                Select Category
+              </Button>
+            </Form>
           </Col>
         </Row>
       </Container>
